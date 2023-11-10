e20686c38d4b9c69a7fece43dcef979a7a46816a
chenjiawei@zhuojianchina.com <chenjiawei@zhuojianchina.com>
2023-11-10 11:40:43 +0800

    注册流程优化

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -2,6 +2,22 @@ const UserService = require("../service/user.service");
 class UserController {
     async create(ctx, next) {
         const user = ctx.request.body;
+        const { name, password } = user
+        if(!name || !password) {
+            ctx.body = {
+                code: -1001,
+                message: '用户名或密码不能为空'
+            }
+            return
+        }
+        const isUser = await UserService.findUserByUsername(name)
+        if(isUser.length) {
+            ctx.body = {
+                code: -1002,
+                message: '用户已经存在'
+            }
+            return
+        }
         const result = await UserService.create(user)
         ctx.body = {
             message: '创建用户成功',
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -4,8 +4,13 @@ class UserService {
     async create(user) {
         const { name, password } = user
         const statement = 'INSERT INTO `admin` (username, password) VALUES (?, ?);'
-        const result = await connection.execute(statement, [name, password])
+        const [result] = await connection.execute(statement, [name, password])
         return result
+    }
+    async findUserByUsername(name) {
+       const statement = 'SELECT * FROM `admin` WHERE username = ?'
+       const [result] = await connection.execute(statement, [name])
+       return result
     }
 }
 
